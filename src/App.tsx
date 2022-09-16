846b848532af5d61c43d3360fb37d8c2ae939578
unknown <usmanmubashir943@gmail.com>
2022-09-16 19:54:41 +0500

    implemented-typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState,createContext } from 'react';
+import {AiOutlinePlus,AiFillDelete,AiOutlineArrowUp,AiOutlineArrowDown} from "react-icons/ai";
 import './App.css';
+import ReactSwitch from "react-switch";
+export const ThemeContext = createContext(null);
 
-function App() {
+const App:React.FC= () =>{
+  
+  const [value,setValue] = useState<string>("");
+  const [items,setItems] = useState<string[]>([]);
+  const [theme,setTheme] = useState<string>("light");
+
+  const deleteItem = (id:number) => {
+    setItems((items)=>{
+      return items.filter((items,index)=>{
+        return index !== id;
+      });
+    });
+  }
+
+  const handleChange = (event :React.ChangeEvent<HTMLInputElement>)=>{
+    let newValue = event.currentTarget.value;
+    setValue(newValue);
+  }
+  const handleClick = ()=>{
+    setItems((items)=>{
+      return[...items,value]
+    })
+  }
+  const move = (firstIndex:number,secondIndex:number) => {
+    let newItems:any[] = [...items];
+    let temp:any = newItems[firstIndex];
+    newItems[firstIndex] = newItems[secondIndex];
+    newItems[secondIndex] = temp;
+    setItems([...newItems]);
+  }
+
+  const toggleTheme = () =>{
+    setTheme((curr)=>(curr === "light" ? "dark" :"light"))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='App' id ={theme}>
+      <h1>TODO App</h1>
+        <div className="switch">
+            <label>{theme === "light" ? "Light Mode" :"Dark Mode"}</label>
+            <ReactSwitch onChange={toggleTheme} height ={20} width={40}checked={theme === "dark"}/>
+        </div>
+        <div className='title' placeholder='Enter Item'><p>Items</p>
+          <button onClick={handleClick}><AiOutlinePlus/></button>
+        </div>
+        
+        <div>{items.map((item,index)=>(
+          <div key = {index} className='items'>
+            <p contentEditable='true'>{item}{" "}</p>
+            <div>
+              <button onClick={()=>{
+                deleteItem(index);
+              }}><AiFillDelete /></button>
+              <button onClick={()=>{
+                move(index,(index)-1)
+              }}><AiOutlineArrowUp /></button>
+              <button onClick={()=>{
+                move(index,(index)+1)
+              }}><AiOutlineArrowDown /></button>
+            </div>
+        </div>
+        ))}</div>
+      </div>
   );
 }
 
